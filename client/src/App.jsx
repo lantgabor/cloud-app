857e35679165360f3601dd64f7987eb03c78094f
lantgabor <lant.gabor98@gmail.com>
2022-05-12 22:24:49 +0200

    moves button

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,8 +14,8 @@ function App() {
                     alignItems: "center",
                 }}
             >
-                <ParkingComponent />
                 <InputComponent />
+                <ParkingComponent />
             </Box>
         </>
     );
--- client/src/components/InputComponent.jsx
+++ client/src/components/InputComponent.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 
-import { Button } from "@mui/material";
+import { Button, TextField } from "@mui/material";
+import Box from "@mui/material/Box";
 
 export function InputComponent() {
     return (
-        <>
+        <Box
+            sx={{
+                display: "flex",
+                justifyContent: "space-around",
+                alignItems: "center",
+            }}
+        >
+            <TextField></TextField>
             <Button>Add button</Button>
-        </>
+        </Box>
     );
 }
